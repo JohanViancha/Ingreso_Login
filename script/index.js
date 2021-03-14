37c1ce05f24e61509bf752061f10c07c78b2d1a7
JohanViancha <40503636+JohanViancha@users.noreply.github.com>
2021-03-14 08:21:53 -0500

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,61 @@
+function iniciar() {
+
+    let us = 'yeila';
+    let pass = '123';
+    let nombre = 'Yeila Ardila';
+
+    var hammertime = new Hammer(ingresar);
+    hammertime.on('pan', function(ev) {
+        console.log(ev);
+    });
+
+
+    usuario.addEventListener('keypress', function(ev) {
+        usuario_mostrar.innerHTML = usuario_mostrar.innerHTML + ev.key;
+    }, false)
+
+
+    password.addEventListener('keypress', function(ev) {
+        password_mostrar.innerHTML = password_mostrar.innerHTML + ev.key;
+    }, false)
+
+    ingresar.addEventListener('click', function(ev) {
+
+
+        ev.preventDefault();
+        console.log();
+        if (usuario.value != "" && password.value != "") {
+
+            if (usuario.value != us || password.value != pass) {
+                Swal.fire({
+                    title: 'Usuario y/o contaseña incorrectos',
+                    text: 'Por favor intente de nuevo',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                })
+            } else {
+                localStorage.setItem('nombre', nombre);
+                usuario.value = "";
+                password.value = "";
+                Swal.fire({
+                    title: '¡Bienvenido!',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                }).then(() => {
+                    location.href = "home.html";
+                })
+
+            }
+
+        } else {
+            Swal.fire({
+                text: 'El usuario y la contraseña son obligatorios',
+                icon: 'warning',
+                confirmButtonText: 'Aceptar'
+            })
+        }
+
+    }, false)
+}
+
+iniciar();
